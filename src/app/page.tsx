3e63327e257e5c74ea696e161a8006ae0d0180fc
Cinquin Andy <cinquin.andy@gmail.com>
2025-01-15 11:53:38 +0100

    fix: prettier config & eslintconfig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Navbar from '../components/Navbar.component.js';
+import Navbar from '../components/Navbar.component.js'
 
 export default function Home() {
-    return (
-        <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <Navbar />
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                test
-            </main>
-        </div>
-    );
+	return (
+		<div className='grid min-h-screen grid-rows-[auto_1fr_auto] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
+			<Navbar />
+			<main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
+				test
+			</main>
+		</div>
+	)
 }
