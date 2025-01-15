68d72be70cf44b3c123995c957dcb1ccf8027863
Killian Gascon <gasconkillian.pro@gmail.com>
2025-01-15 14:56:52 +0100

    feat : Navbar creation, added background video (not the final one) and changed the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import Navbar from '../components/Navbar.component.js'
+import Navbar from '../components/Navbar.component.js';
 
 export default function Home() {
 	return (
-		<div className='grid min-h-screen grid-rows-[auto_1fr_auto] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
+		<div className='relative w-full h-screen items-center justify-center'>
 			<Navbar />
-			<main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
-				test
+
+			<main className='w-full h-full flex flex-col items-center gap-8 pt-20 sm:items-start'>
+				<div className='w-full flex flex-col items-center gap-16'>
+					{/*<video*/}
+					{/*	className='w-full flex flex-col items-center gap-16'*/}
+					{/*	src="/assets/videos/AnimationCarte.mp4"*/}
+					{/*	autoPlay*/}
+					{/*	loop*/}
+					{/*	muted*/}
+					{/*/>*/}
+				</div>
 			</main>
 		</div>
-	)
+	);
 }
