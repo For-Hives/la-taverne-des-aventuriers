0781d7a6bf3be478e312281dfe438bda3546a50d
Cinquin Andy <cinquin.andy@gmail.com>
2025-01-16 15:34:13 +0100

    feat: Refactor code structure and improve imports
    - Cleaned up import statements for better readability.
    - Added a new TypeScript command to package.json for easier type checking.
    - Removed unnecessary entries from .gitignore.
    - Adjusted Tailwind CSS configuration for consistent styling.
    - Made minor layout adjustments in the main component for improved responsiveness.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,35 +9,35 @@ export default {
 	plugins: [],
 	theme: {
 		borderWidth: {
-			DEFAULT: '1px',
 			'0': '0',
 			'2': '2px',
 			'3': '3px',
 			'4': '4px',
 			'6': '6px',
 			'8': '8px',
+			DEFAULT: '1px',
 		},
 		extend: {
-			fontFamily: {
-				cardinal: ['"Cardinal"', "sans-serif"],
-				kcobraletra: ['"KC Obra Letra"', "sans-serif"]
-			},
-
-			colors: {
-				background: 'var(--background)',
-				foreground: 'var(--foreground)',
-				'title':{
-					100: '#8D2026',
-					200: '#582615',
-					300: '#FFFCEE'
-				}
-			},
 			backgroundImage: {
 				'lp-card1-bg': "url('/assets/images/vinbiere.png')",
 				'lp-card2-bg': "url('/assets/images/vinbiere.png')",
 				'lp-card3-bg': "url('/assets/images/vinbiere.png')",
 				// 'lp-hero-bg': "url('/assets/videos/AnimationCarte.mp4')",
-			}
+			},
+
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				title: {
+					100: '#8D2026',
+					200: '#582615',
+					300: '#FFFCEE',
+				},
+			},
+			fontFamily: {
+				cardinal: ['"Cardinal"', 'sans-serif'],
+				kcobraletra: ['"KC Obra Letra"', 'sans-serif'],
+			},
 		},
 	},
 } satisfies Config
