ab45ee2089050a732e29bcb780ed9a69b8645ca7
Killian Gascon <gasconkillian.pro@gmail.com>
2025-01-16 16:08:43 +0100

    fix: dependencies versions modified

--- src/components/Reservation_Card.js
+++ src/components/Reservation_Card.js
@@ -1,27 +1,27 @@
-import Link from 'next/link';
+import Link from 'next/link'
 // import Image from 'next/image';
 // import Navbar from "./Navbar.component";
 
 const Reservation_Card = () => {
-    return (
-        <div className=' bg-gray-700 py-5 w-3/4 mx-auto p-10 rounded-lg gap-20'>
-            <div className='items-left'>
-                <h1 className="text-left">Comment Réserver ?</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut turpis dictum, feugiat justo nec,
-                    ultrices urna. Curabitur ut ipsum et libero pretium viverra. Nullam sed lacus enim.
-                    Sed tincidunt tincidunt urna, in lacinia ipsum ullamcorper eu. Nunc tempus eget augue at interdum.
-                    Aliquam in maximus nisl. Duis accumsan venenatis dui, dignissim congue leo scelerisque in.
-                    Sed hendrerit efficitur viverra.
-                </p>
-                <button className='bg-amber-100 rounded-lg'>
-                    <Link href='#' className='hover:underline text-gray-800'>
-                        Réservez
-                    </Link>
-                </button>
-            </div>
-
-        </div>
-    )
+	return (
+		<div className='mx-auto w-3/4 gap-20 rounded-lg bg-gray-700 p-10 py-5'>
+			<div className='items-left'>
+				<h1 className='text-left'>Comment Réserver ?</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut
+					turpis dictum, feugiat justo nec, ultrices urna. Curabitur ut ipsum et
+					libero pretium viverra. Nullam sed lacus enim. Sed tincidunt tincidunt
+					urna, in lacinia ipsum ullamcorper eu. Nunc tempus eget augue at
+					interdum. Aliquam in maximus nisl. Duis accumsan venenatis dui,
+					dignissim congue leo scelerisque in. Sed hendrerit efficitur viverra.
+				</p>
+				<button className='rounded-lg bg-amber-100'>
+					<Link href='#' className='text-gray-800 hover:underline'>
+						Réservez
+					</Link>
+				</button>
+			</div>
+		</div>
+	)
 }
-export default Reservation_Card;
+export default Reservation_Card
--- src/components/lp_Background_Video.js
+++ src/components/lp_Background_Video.js
@@ -1,63 +1,65 @@
 // client side
-"use client";
+'use client'
 
+import Player from '@vimeo/player'
 // Imports
-import React, { useEffect, useRef } from "react";
-import Player from "@vimeo/player";
+import React, { useEffect, useRef } from 'react'
 
 // Declare the Video manager
 const BackgroundVideoLP = () => {
-    const firstVideoRef = useRef(null);
+	const firstVideoRef = useRef(null)
 
-    useEffect(() => {
-        // Verify if Vimeo iframe exist
-        if (!firstVideoRef.current) return;
+	useEffect(() => {
+		// Verify if Vimeo iframe exist
+		if (!firstVideoRef.current) return
 
-        // assignment of the Iframe to a player (vimeo API)
-        const firstPlayer = new Player(firstVideoRef.current);
+		// assignment of the Iframe to a player (vimeo API)
+		const firstPlayer = new Player(firstVideoRef.current)
 
-        // Variables to set the loop time and Vimeo Video duration
-        let videoDuration = 0;
-        const loopStart = 4; // Last three seconds of the video
+		// Variables to set the loop time and Vimeo Video duration
+		let videoDuration = 0
+		const loopStart = 4 // Last three seconds of the video
 
-        // Listener for the event 'durationchange' to obtain the video duration
-        firstPlayer.on("durationchange", (data) => {
-            // Assigning the video duration time to a variable
-            videoDuration = data.duration;
-        });
+		// Listener for the event 'durationchange' to obtain the video duration
+		firstPlayer.on('durationchange', data => {
+			// Assigning the video duration time to a variable
+			videoDuration = data.duration
+		})
 
-        // Listener for the event 'ended' to begin the loop right after the video ended
-        firstPlayer.on("ended", () => {
-            // Come back to the ... last seconds of the video to begin a loop
-            firstPlayer.setCurrentTime(videoDuration - loopStart);
-            firstPlayer.play(); // Begin the loop
-        });
+		// Listener for the event 'ended' to begin the loop right after the video ended
+		firstPlayer.on('ended', () => {
+			// Come back to the ... last seconds of the video to begin a loop
+			firstPlayer.setCurrentTime(videoDuration - loopStart)
+			firstPlayer.play() // Begin the loop
+		})
 
-        // Cleaning
-        return () => {
-            firstPlayer.destroy().catch((error) => console.error("Error Destruction of the player", error));
-        };
-    }, []);
+		// Cleaning
+		return () => {
+			firstPlayer
+				.destroy()
+				.catch(error => console.error('Error Destruction of the player', error))
+		}
+	}, [])
 
-    return (
-            // Vimeo Iframe
-            <iframe
-                // indicating the video
-                ref={firstVideoRef}
-                // Styles
-                className="mask-custom absolute left-0 top-0 object-cover object-center mix-difference animate-video -z-10 block bg-slate-900 opacity-75"
-                // Url + set Video to Background, allow autoplay and disable the full video loop
-                src="https://player.vimeo.com/video/1047422333?h=3ee0913fe6&background=1&autoplay=1&loop=0"
-                // title
-                title="Animation_Carte_1"
-                // Dimensions of the Frame
-                width="1920"
-                height="1080"
-                // Allowing Fullscreen mode
-                allowFullScreen
-            ></iframe>
-    );
-};
+	return (
+		// Vimeo Iframe
+		<iframe
+			// indicating the video
+			ref={firstVideoRef}
+			// Styles
+			className='mask-custom mix-difference animate-video absolute left-0 top-0 -z-10 block bg-slate-900 object-cover object-center opacity-75'
+			// Url + set Video to Background, allow autoplay and disable the full video loop
+			src='https://player.vimeo.com/video/1047422333?h=3ee0913fe6&background=1&autoplay=1&loop=0'
+			// title
+			title='Animation_Carte_1'
+			// Dimensions of the Frame
+			width='1920'
+			height='1080'
+			// Allowing Fullscreen mode
+			allowFullScreen
+		></iframe>
+	)
+}
 
 // export
-export default BackgroundVideoLP;
+export default BackgroundVideoLP
