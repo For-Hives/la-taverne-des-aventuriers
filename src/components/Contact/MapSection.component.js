f0442618516ed0b1fc50619a9724d7dc781c0447
Killian Gascon <gasconkillian.pro@gmail.com>
2025-01-20 09:32:41 +0100

    feat: modified contact Page

---
+++ src/components/Contact/MapSection.component.js
@@ -0,0 +1,40 @@
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const MapSection = () => {
+	return (
+		<div className='flex w-full items-center gap-8 font-obraletra text-title-200'>
+			<div className='flex w-1/3 flex-col items-start gap-16 p-16'>
+				<h1 className='font-cardinal text-6xl first-letter:text-title-100'>
+					Adresse
+				</h1>
+				<p className='w-2/3 text-5xl'>13 rue Kergévan, 44000 Nantes</p>
+				<button className='flex w-1/3 items-center justify-center rounded bg-title-200 p-1 font-cardinal text-xl'>
+					<Link
+						target='_blank'
+						href='https://maps.app.goo.gl/ciyAbT5PPQZxbxcUA'
+						className='flex items-center gap-3 text-xl text-title-300'
+					>
+						Google Maps
+						<FontAwesomeIcon
+							icon={faChevronRight}
+							className='h-4 text-title-300'
+						/>
+					</Link>
+				</button>
+			</div>
+			<div className='w-2/3'>
+				<Image
+					src='/assets/images/elements/ContactElements/maps.png'
+					alt='LTDA Logo'
+					width={1920}
+					height={1080}
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default MapSection
