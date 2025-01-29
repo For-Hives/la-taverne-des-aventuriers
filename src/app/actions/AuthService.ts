1723aeff52e5d8e584735bfc54160ee08f540ae6
Killian Gascon <gasconkillian.pro@gmail.com>
2025-01-29 11:10:29 +0100

    feat: modified connection type

--- src/app/actions/AuthService.ts
+++ src/app/actions/AuthService.ts
@@ -1,30 +1,17 @@
 import 'server-only' // Indique que ce fichier doit être exécuté côté serveur uniquement
 import PocketBase from 'pocketbase' // Importe la bibliothèque PocketBase pour l'utiliser
 
-/**
- * Authenticate with PocketBase using email and password
- * @returns {Promise<PocketBase | null>} Promise résolvant avec l'instance PocketBase ou null si l'authentification échoue
- */
 export const authWithPocketBase = async (): Promise<PocketBase | null> => {
-	// Récupère les variables d'environnement nécessaires pour l'authentification
-	const pb_env = {
-		PB_SERVER_URL: process.env.PB_SERVER_URL, // URL du serveur PocketBase
-		PB_USER_EMAIL: process.env.PB_USER_EMAIL, // Email de l'utilisateur pour l'authentification
-		PB_USER_PASSWORD: process.env.PB_USER_PASSWORD, // Mot de passe de l'utilisateur pour l'authentification
+	const token = process.env.PB_USER_TOKEN
+	const url = process.env.PB_SERVER_URL
+
+	if (!token) {
+		return null
 	}
 
-	// Crée une instance de PocketBase en utilisant l'URL du serveur
-	const pb = new PocketBase(pb_env.PB_SERVER_URL)
+	const pb = new PocketBase(url)
 
-	// Vérifie si les informations d'identification (email et mot de passe) sont présentes
-	if (pb_env.PB_USER_EMAIL && pb_env.PB_USER_PASSWORD) {
-		// Authentifie l'utilisateur avec les informations fournies
-		await pb
-			.collection('users') // Sélectionne la collection des utilisateurs
-			.authWithPassword(pb_env.PB_USER_EMAIL, pb_env.PB_USER_PASSWORD) // Authentifie avec l'email et le mot de passe
-		// Si l'authentification est réussie, retourne l'instance PocketBase
-		return pb
-	}
-	// Si les informations de connexion sont manquantes ou invalides, retourne null
-	return null
+	pb.authStore.save(token, null)
+
+	return pb
 }
--- src/components/Global/ReservationCard.component.tsx
+++ src/components/Global/ReservationCard.component.tsx
@@ -1,5 +1,5 @@
 import { getReservationCardData } from '@/app/actions/services/getReservationCardData.service'
-import { textToSpanColored } from '@/utils/textToSpanColored'
+// import { textToSpanColored } from '@/utils/textToSpanColored'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -8,48 +8,50 @@ export default async function ReservationCardComponent() {
 	const data = await getReservationCardData()
 
 	// Structure the data into a usable object
-	const ReservationData = {
-		buttonLabel: data.button_label,
-		buttonUrl: data.button_url,
-		description: textToSpanColored(data.description),
-		gamesUrl: data.games_url,
-		image: data.image,
-		title: textToSpanColored(data.Title),
-	}
+	// const ReservationData = {
+	// 	buttonLabel: data.button_label,
+	// 	buttonUrl: data.button_url,
+	// 	description: textToSpanColored(data.description),
+	// 	gamesUrl: data.games_url,
+	// 	image: data.image,
+	// 	title: textToSpanColored(data.Title),
+	// }
+
+	console.info(data)
 
 	return (
-		<div className='bg-customWhite-500 mx-auto flex w-full max-w-6xl flex-col items-center gap-8 rounded p-4 sm:flex-row sm:gap-20 sm:p-10'>
+		<div className='mx-auto flex w-full max-w-6xl flex-col items-center gap-8 rounded bg-customWhite-500 p-4 sm:flex-row sm:gap-20 sm:p-10'>
 			{/* Text Section */}
-			<div className='text-customBrown-100 flex w-full flex-col gap-6 p-4 font-obraletra text-base sm:w-2/3 sm:gap-9 sm:p-8'>
+			<div className='flex w-full flex-col gap-6 p-4 font-obraletra text-base text-customBrown-100 sm:w-2/3 sm:gap-9 sm:p-8'>
 				{/* Title */}
-				<h1 className='first-letter:text-customRed-100 text-left font-cardinal text-xl sm:text-2xl'>
-					{ReservationData.title}
+				<h1 className='text-left font-cardinal text-xl first-letter:text-customRed-100 sm:text-2xl'>
+					{data.Title}
 				</h1>
 
 				{/* Description (rendering HTML content) */}
 				<div
 					dangerouslySetInnerHTML={{
-						__html: ReservationData.description,
+						__html: data.description,
 					}}
 				></div>
 
 				{/* Link to the games URL */}
 				<Link
-					href={ReservationData.gamesUrl}
-					className='text-customBrown-100 text-xs hover:underline sm:text-base'
+					href={data.games_url}
+					className='text-xs text-customBrown-100 hover:underline sm:text-base'
 				>
-					{ReservationData.gamesUrl}
+					{data.games_url}
 				</Link>
 
 				{/* Button Section */}
 				<div>
-					<button className='bg-customWhite-100 rounded p-2'>
+					<button className='rounded bg-customWhite-100 p-2'>
 						{/* Button Link */}
 						<Link
-							href={ReservationData.buttonLabel}
-							className='text-customBrown-100 text-xs hover:underline sm:text-base'
+							href={data.button_url}
+							className='text-xs text-customBrown-100 hover:underline sm:text-base'
 						>
-							{ReservationData.buttonLabel}
+							{data.button_label}
 						</Link>
 					</button>
 				</div>
