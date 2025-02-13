fa1165f39a2263ab45e8e85e10fff104cf3edce1
Killian Gascon <gasconkillian.pro@gmail.com>
2025-02-13 15:42:56 +0100

    feat: added 404 page

--- src/components/animation/RiveAnimation.tsx
+++ src/components/animation/RiveAnimation.tsx
@@ -1,36 +1,14 @@
-import { useRive } from '@rive-app/react-canvas'
-import { useEffect, useState } from 'react'
+import { useRive } from '@rive-app/react-canvas';
 
 export default function RiveAnimation() {
-	const [isMobile, setIsMobile] = useState(false)
+    const { rive, RiveComponent } = useRive({
+        src: '/assets/animation/chevalier.riv',
+        stateMachines: "bumpy",
+        autoplay: true,
 
-	useEffect(() => {
-		const mediaQuery = window.matchMedia('(max-width: 768px)')
-		const handleChange = () => setIsMobile(mediaQuery.matches)
+    });
 
-		handleChange()
-		mediaQuery.addEventListener('change', handleChange)
-
-		return () => mediaQuery.removeEventListener('change', handleChange)
-	}, [])
-
-	const { RiveComponent: RiveDesktopComponent } = useRive({
-		automaticallyHandleEvents: true,
-		autoplay: true,
-		isTouchScrollEnabled: true,
-		src: '/assets/animation/chevalier.riv',
-		stateMachines: 'State Machine 1',
-
-
-	})
-
-	const { RiveComponent: RiveMobileComponent } = useRive({
-		automaticallyHandleEvents: true,
-		autoplay: true,
-		isTouchScrollEnabled: true,
-		src: '/assets/animation/chevaliermobile.riv',
-		stateMachines: 'State Machine 1',
-	})
-
-	return <>{isMobile ? <RiveMobileComponent /> : <RiveDesktopComponent />}</>
+    return (
+        <RiveComponent />
+    );
 }
