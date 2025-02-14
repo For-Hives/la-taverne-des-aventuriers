accfdda0b2d39481b18eb5c604462edb67a2ce6f
Killian Gascon <gasconkillian.pro@gmail.com>
2025-02-14 15:15:35 +0100

    feat: modified 404 page animation

--- src/components/animation/RiveAnimation.tsx
+++ src/components/animation/RiveAnimation.tsx
@@ -3,7 +3,7 @@ import { useRive } from '@rive-app/react-canvas';
 export default function RiveAnimation() {
     const { rive, RiveComponent } = useRive({
         src: '/assets/animation/chevalier.riv',
-        stateMachines: "bumpy",
+        stateMachines: "State Machine 1",
         autoplay: true,
 
     });
