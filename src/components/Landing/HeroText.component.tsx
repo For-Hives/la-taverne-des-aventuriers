f5d1880d6f3e53295923cd1310bfc7ea4f28a153
CinquinAndy <cinquin.andy@gmail.com>
2025-02-18 22:24:17 +0100

    feat(ui): add Cardinal font and update layout
    - Introduce Cardinal font files for use in the app
    - Update layout to include Cardinal font variable
    - Modify Tailwind config to reference Cardinal font correctly
    - Clean up unused imports in HeroText component

--- src/components/Landing/HeroText.component.tsx
+++ src/components/Landing/HeroText.component.tsx
@@ -1,9 +1,5 @@
 import { LandingPageData } from '@/app/actions/services/getLandingPageData.service'
 import { HeroTextAnimated } from '@/components/Landing/animatedComponents/heroText'
-// import { textToSpanColored } from '@/utils/textToSpanColored'
-// import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import Link from 'next/link'
 
 export default async function HeroTextComponent({
 	data,
