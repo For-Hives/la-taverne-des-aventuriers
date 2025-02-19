3360fdf84a920a6b1b7be91e2ad2b995568c9fbd
CinquinAndy <cinquin.andy@gmail.com>
2025-02-19 23:52:31 +0100

    feat(ui): enhance event page layout and styling
    - Adjust padding for main content area
    - Update opacity transformation for scroll effect
    - Modify timeline component styles for better alignment
    - Increase max width of event details section
    - Add responsive image handling for desktop and mobile views
    - Improve visibility of event summaries with updated font weights

--- src/components/Events/Timeline.tsx
+++ src/components/Events/Timeline.tsx
@@ -33,16 +33,16 @@ export const Timeline = ({ events }: { events: TimelineEntry[] }) => {
 	})
 
 	const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height])
-	const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1])
+	const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 0.4])
 
 	return (
 		<div className='relative w-full font-sans' ref={containerRef}>
-			<div className='mx-auto max-w-7xl pb-20'>
+			<div className='relative mx-auto max-w-7xl pb-20'>
 				<div
 					style={{
 						height: height + 'px',
 					}}
-					className='absolute left-4 top-0 w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-customBrown-100/20 to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] lg:left-8'
+					className='absolute left-[18px] top-0 w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-customBrown-100/20 to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] lg:left-[-12px]'
 				>
 					<motion.div
 						style={{
@@ -70,7 +70,7 @@ export const Timeline = ({ events }: { events: TimelineEntry[] }) => {
 									})}
 								</h3>
 							</div>
-							<div className='relative w-full max-w-2xl pl-4'>
+							<div className='relative w-full max-w-5xl pl-4'>
 								<motion.div
 									initial={{ opacity: 0, y: 20 }}
 									whileInView={{ opacity: 1, y: 0 }}
@@ -78,28 +78,63 @@ export const Timeline = ({ events }: { events: TimelineEntry[] }) => {
 									viewport={{ once: true }}
 									className='rounded-lg border border-customBrown-100/20 bg-white p-6 shadow-lg transition-all hover:border-customBrown-100/40'
 								>
-									<h4 className='mb-4 font-cardoRegular text-xl text-customBrown-100'>
+									<h4 className='mb-4 font-cardoRegular text-xl font-bold text-customBrown-100 md:text-2xl lg:hidden'>
 										{event.title}
 									</h4>
 
+									{/* Desktop */}
+									<div className={'flex gap-8'}>
+										{event.image && (
+											<div className='relative hidden h-auto min-h-96 w-full overflow-hidden rounded-lg lg:block'>
+												<Image
+													src={event.image}
+													alt={event.title}
+													fill
+													className='object-cover transition-transform duration-300 hover:scale-105'
+												/>
+											</div>
+										)}
+										<div className={'flex flex-col justify-between'}>
+											<div>
+												<h4 className='mb-4 hidden font-cardoRegular text-3xl font-bold text-customBrown-100 md:block'>
+													{event.title}
+												</h4>
+												<div
+													className='prose mb-4 hidden font-cardoRegular text-customBrown-100 lg:block'
+													dangerouslySetInnerHTML={{ __html: event.summary }}
+												/>
+											</div>
+
+											<div>
+												<Link
+													href={`/evenements/${event.slug}`}
+													className='hidden rounded-md bg-customBrown-100 px-4 py-2 text-white transition-colors duration-300 hover:bg-customBrown-100/80 lg:inline-block'
+												>
+													En savoir plus
+												</Link>
+											</div>
+										</div>
+									</div>
+
+									{/*Mobile / tablet */}
 									{event.image && (
-										<div className='relative mb-4 h-48 w-full overflow-hidden rounded-lg'>
+										<div className='relative mb-4 h-72 w-full overflow-hidden rounded-lg md:h-96 lg:hidden'>
 											<Image
 												src={event.image}
 												alt={event.title}
 												fill
-												className='object-cover transition-transform duration-300 hover:scale-105'
+												className='object-cover transition-transform duration-300 hover:scale-105 lg:hidden'
 											/>
 										</div>
 									)}
 
 									<div
-										className='mb-4 font-cardoRegular text-customBrown-100'
+										className='prose mb-4 font-cardoRegular text-customBrown-100 lg:hidden'
 										dangerouslySetInnerHTML={{ __html: event.summary }}
 									/>
 									<Link
 										href={`/evenements/${event.slug}`}
-										className='inline-block rounded-md bg-customBrown-100 px-4 py-2 text-white transition-colors duration-300 hover:bg-customBrown-100/80'
+										className='inline-block rounded-md bg-customBrown-100 px-4 py-2 text-white transition-colors duration-300 hover:bg-customBrown-100/80 lg:hidden'
 									>
 										En savoir plus
 									</Link>
