a0ded963139c0aaedfc1e8180b84ba33eb8dc71d
CinquinAndy <cinquin.andy@gmail.com>
2025-02-24 21:06:17 +0100

    feat(ui): enhance animated reservation layout
    - Update grid layout for better responsiveness
    - Adjust animation properties for smoother transitions
    - Modify image handling to improve loading and display
    - Refine card styles for consistent appearance across breakpoints
    - Optimize background gradient overlays for visual appeal

--- src/components/Reservation/animatedComponents/AnimatedReservationElement.tsx
+++ src/components/Reservation/animatedComponents/AnimatedReservationElement.tsx
@@ -12,25 +12,34 @@ export const AnimatedReservationElement = ({
 	return (
 		<div className='flex h-full w-11/12 items-center justify-center font-cardoRegular text-customBrown-100'>
 			{/* Grid layout for the content */}
-			<div className='grid h-full w-full grid-cols-1 grid-rows-8 gap-4 max-xl:grid-rows-4 max-md:grid-rows-5 sm:grid-cols-2 xl:grid-cols-10'>
+			<div className='grid h-full w-full grid-cols-1 grid-rows-8 gap-4 max-lg:grid-rows-1 max-md:grid-rows-1 sm:grid-cols-2 lg:grid-cols-10'>
 				{/* Card 1 - Top Left */}
 				<motion.div
-					className='col-span-1 row-span-1 flex items-center justify-start rounded-xl border border-customBrown-100 bg-customWhite-300 sm:row-span-1 md:col-span-2 xl:col-span-8 xl:row-span-3'
+					className='col-span-1 row-span-1 flex items-center justify-start rounded-xl border border-customBrown-100 bg-customWhite-300 sm:row-span-1 md:col-span-2 lg:col-span-8 lg:row-span-3'
 					initial={{ opacity: 0, y: 50 }} // Initial opacity and vertical position for animation
 					animate={{ opacity: 1, y: 0 }} // Animate to full opacity and original vertical position
 					transition={{ delay: 0.2, duration: 0.8 }} // Delay and duration for the animation
 				>
 					<div className='relative flex h-full w-full items-start justify-start overflow-hidden rounded-xl p-5 sm:p-8'>
 						{/* Image with Next.js Image component */}
-						<Image
-							src={data.image_card1}
-							alt='Image description'
-							layout='fill'
-							objectFit='cover'
-							objectPosition='center'
-							className='absolute inset-0 z-0'
-							quality={100}
-						/>
+						<div
+							className={
+								'absolute right-0 top-0 hidden h-full lg:flex lg:w-4/6'
+							}
+						>
+							<div className='relative h-full w-full'>
+								<Image
+									src={data.image_card1}
+									alt='Image description'
+									fill={true}
+									objectFit={'cover'}
+									objectPosition={'center'}
+									className='absolute inset-0 z-0'
+									quality={80}
+								/>
+							</div>
+						</div>
+
 						{/* Gradient overlay for the background */}
 						<div className='absolute inset-0 z-0 bg-gradient-to-r from-customWhite-300/100 via-customWhite-300/100 to-customWhite-300/20' />
 
@@ -49,7 +58,7 @@ export const AnimatedReservationElement = ({
 
 				{/* Card 2 - Middle Right */}
 				<motion.div
-					className='relative col-span-1 row-span-1 flex flex-col items-start justify-start rounded-xl border border-customBrown-100 bg-customWhite-300 md:col-span-1 md:row-span-4 xl:col-span-2'
+					className='relative col-span-1 flex flex-col items-start justify-start rounded-xl border border-customBrown-100 bg-customWhite-300 md:col-span-1 md:row-span-4 lg:col-span-2'
 					initial={{ opacity: 0, x: -50 }} // Initial opacity and horizontal position for animation
 					animate={{ opacity: 1, x: 0 }} // Animate to full opacity and original horizontal position
 					transition={{ delay: 0.4, duration: 0.8 }} // Delay and duration for the animation
@@ -64,17 +73,17 @@ export const AnimatedReservationElement = ({
 						/>
 					</div>
 					<Image
-						className='absolute bottom-0 right-0 z-40 w-3/6 translate-x-20 translate-y-20 max-xl:hidden'
+						className='absolute bottom-0 right-0 z-40 w-[170px] translate-x-20 translate-y-20 max-lg:hidden'
 						src='/assets/images/elements/WhoAreWeElement/dice.webp' // Dice image source
 						alt='Dice' // Alt text for accessibility
-						width={520}
-						height={520}
+						width={250}
+						height={250}
 					/>
 				</motion.div>
 
 				{/* Card 3 - Middle Left */}
 				<motion.div
-					className='col-span-1 row-span-1 flex flex-col items-start justify-start rounded-xl border border-customBrown-100 bg-customWhite-300 md:col-span-1 md:row-span-4 xl:col-span-2 xl:row-span-8'
+					className='col-span-1 row-span-1 flex flex-col items-start justify-start rounded-xl border border-customBrown-100 bg-customWhite-300 md:col-span-1 md:row-span-4 lg:col-span-2 lg:row-span-8'
 					initial={{ opacity: 0, x: 50 }} // Initial opacity and horizontal position for animation
 					animate={{ opacity: 1, x: 0 }} // Animate to full opacity and original horizontal position
 					transition={{ delay: 0.6, duration: 0.8 }} // Delay and duration for the animation
@@ -92,7 +101,7 @@ export const AnimatedReservationElement = ({
 
 				{/* Central Logo */}
 				<motion.div
-					className='bg-tan-200 relative col-span-1 row-span-3 flex items-center justify-center rounded-xl border border-customBrown-100 bg-customWhite-300 max-xl:hidden md:col-span-2 md:row-span-10 xl:col-span-6'
+					className='bg-tan-200 relative col-span-1 row-span-3 flex items-center justify-center rounded-xl border border-customBrown-100 bg-customWhite-300 max-lg:hidden md:col-span-2 md:row-span-10 lg:col-span-6'
 					initial={{ opacity: 0 }} // Initial opacity for animation
 					animate={{ opacity: 1 }} // Animate to full opacity
 					transition={{ delay: 0.8, duration: 0.8 }} // Delay and duration for the animation
@@ -105,7 +114,7 @@ export const AnimatedReservationElement = ({
 					/>
 
 					<Image
-						className='absolute bottom-0 right-0 w-1/6 translate-x-10 translate-y-24'
+						className='absolute bottom-0 right-0 w-[170px] translate-x-10 translate-y-24'
 						src='/assets/images/elements/WhoAreWeElement/dice.webp' // Dice image source
 						alt='Dice' // Alt text for accessibility
 						width={520}
@@ -115,13 +124,13 @@ export const AnimatedReservationElement = ({
 
 				{/* Card 4 - Bottom Left */}
 				<motion.div
-					className='col-span-1 row-span-1 flex items-start justify-start rounded-xl border border-customBrown-100 bg-customWhite-300 md:col-span-2 md:row-span-9 xl:col-span-2'
+					className='col-span-1 row-span-1 flex items-start justify-start rounded-xl border border-customBrown-100 bg-customWhite-300 md:col-span-2 md:row-span-9 lg:col-span-2'
 					initial={{ opacity: 0, y: 50 }} // Initial opacity and vertical position for animation
 					animate={{ opacity: 1, y: 0 }} // Animate to full opacity and original vertical position
 					transition={{ delay: 1, duration: 0.8 }} // Delay and duration for the animation
 				>
 					<div
-						className='relative flex h-full w-full items-start justify-start overflow-hidden rounded-xl'
+						className='relative flex h-full w-full items-start justify-start overflow-hidden rounded-xl max-lg:!bg-none'
 						style={{
 							backgroundImage: `url(${data.image_card5})`, // Dynamic background image from data
 							backgroundPosition: 'center',
@@ -129,7 +138,7 @@ export const AnimatedReservationElement = ({
 						}}
 					>
 						{/* Gradient overlay for the background */}
-						<div className='absolute inset-0 z-0 bg-gradient-to-b from-customWhite-300/100 via-customWhite-300/100 to-customWhite-300/20 max-xl:bg-gradient-to-r' />
+						<div className='absolute inset-0 z-0 bg-gradient-to-b from-customWhite-300/100 via-customWhite-300/100 to-customWhite-300/20 max-lg:bg-gradient-to-r' />
 
 						{/* Content */}
 						<div className='relative z-10 flex flex-col gap-4 p-8'>
